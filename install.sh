c8d4fecc79487f5db55ef92de073b288f7cd57f6
Mohammed Jawabri <54251668+jxbt@users.noreply.github.com>
2024-05-26 01:28:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get install -y python3-pip python3-venv
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb || true
 apt --fix-broken install
